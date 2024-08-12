106a49690c968464bb94ac7935b55000dd0078ce
masangonathi <nkosinathimasango@hotmail.com>
2024-08-12 21:31:17 +0200

    corrected cases

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <div>
-            <header style ={{ backgroundcolor: 'navy', color: 'white', textAlign: 'center'}}>
+            <header style ={{ backgroundColor: 'navy', color: 'white', textAlign: 'center'}}>
                 <h1>
                     My Favorite Cities
                 </h1>
