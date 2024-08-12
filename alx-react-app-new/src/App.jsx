26d76c7ed2dc9033eb54f900d19ec72d646cd5fc
masangonathi <nkosinathimasango@hotmail.com>
2024-08-12 21:20:29 +0200

    added style to files

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import React from 'react';
-import UserProfile from './UserProfile'
+import UserProfile from './components/UserProfile'
 
 
 
