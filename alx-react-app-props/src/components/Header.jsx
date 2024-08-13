c7292050d7a02d48896c27ed2f5f5193bea90007
masangonathi <nkosinathimasango@hotmail.com>
2024-08-13 11:09:51 +0200

    added new file

---
+++ alx-react-app-props/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <div>
+            <header style ={{ backgroundcolor: 'navy', color: 'white', textAlign: 'center'}}>
+                <h1>
+                    My Favorite Cities
+                </h1>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
