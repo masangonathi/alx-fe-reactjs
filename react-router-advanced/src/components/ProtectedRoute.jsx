9b0bab3d041158c7792abf15246df9b7e9f7b5b5
masangonathi <nkosinathimasango@hotmail.com>
2024-09-01 19:54:48 +0200

    changed file

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,12 @@
+// src/components/ProtectedRoute.jsx
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../auth';
 
-function ProtectedRoute({ isAuthenticated, children }) {
-  return isAuthenticated ? children : <Navigate to="/login" />;
+function ProtectedRoute({ element: Component, ...rest }) {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? <Component {...rest} /> : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
