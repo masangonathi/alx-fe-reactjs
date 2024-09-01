8d9fc9465752c1248eb65f7843e79983e78cd984
masangonathi <nkosinathimasango@hotmail.com>
2024-09-01 19:47:59 +0200

    new file

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,13 +1,24 @@
+// src/components/Profile.jsx
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
-function Profile() {
+const Profile = () => {
   return (
     <div>
-      <h1>Profile Page</h1>
-      <Outlet /> {/* This renders nested routes */}
+      <h2>Profile Page</h2>
+      <nav>
+        <Link to="details">Profile Details</Link>
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default Profile;
