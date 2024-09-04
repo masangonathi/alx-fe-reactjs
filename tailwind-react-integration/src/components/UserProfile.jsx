1fbfa6f9f09dae48a9898058786eaf7e40c73ca0
masangonathi <nkosinathimasango@hotmail.com>
2024-09-04 19:42:11 +0200

    made changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-      <div className="user-profile bg-grey-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img 
             src="https://via.placeholder.com/150" 
             alt="User"
