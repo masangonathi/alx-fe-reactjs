a8524b666b3178f5d90a866cfe91332652b78b35
masangonathi <nkosinathimasango@hotmail.com>
2024-09-05 08:57:10 +0200

    modified file

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
         <img 
             src="https://via.placeholder.com/150" 
             alt="User"
-            className="rounded-full sm:w-24 h-24 sm:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" 
+            className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" 
             />
         <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">John Doe</h1>
         <p className="sm:text-sm md:text-base text-gray-600 text-center">
