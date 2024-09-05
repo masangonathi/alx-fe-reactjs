9893eb41358a79632629527f1133d9eae7f3bb7c
masangonathi <nkosinathimasango@hotmail.com>
2024-09-05 08:54:04 +0200

    made chnages

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+      <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
         <img 
             src="https://via.placeholder.com/150" 
             alt="User"
-            className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" 
+            className="rounded-full sm:w-24 h-24 sm:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" 
             />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">John Doe</h1>
-        <p className="text-sm sm:text-base text-gray-600 text-center">
+        <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">John Doe</h1>
+        <p className="sm:text-sm md:text-base text-gray-600 text-center">
             Developer at Example Co. Loves to write code and explore new technologies.
         </p>
       </div>
